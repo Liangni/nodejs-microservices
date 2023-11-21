709235dffa1b8a1bd75aa6a4a28c651a954bbcd2
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-11-21 18:03:46 +0800

    Resolver update

--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -1,5 +1,31 @@
+const mockMails = [
+    {
+        subject: 'My first Email',
+        reciever: 'test@test.com',
+        content: 'hello world'
+    },
+    {
+        subject: 'My second Email',
+        reciever: 'test@test.com',
+        content: 'hello world'
+    },
+    {
+        subject: 'My third Email',
+        reciever: 'test@test.com',
+        content: 'hello world'
+    }
+]
+
 module.exports = {
     Query: {
-        hey: () => 'hey there'
+        mails: () => mockMails,
+        mail:(_, args) => mockMails[0],
+    },
+    Mutation: {
+        mail: (_, args) => {
+            mockMails[0] = args
+
+            return args
+        }
     }
 }
