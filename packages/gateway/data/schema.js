10176d2b01952032ce7b60da2df42b94eab3c5e5
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-11-21 17:22:58 +0800

    typeDefs update

--- packages/gateway/data/schema.js
+++ packages/gateway/data/schema.js
@@ -2,7 +2,21 @@ const { makeExecutableSchema } = require('graphql-tools')
 const resolvers = require('./resolvers')
 
 const typeDefs = `
-    type Query { hey: String! }
+    type Query {
+        mails: [Mail]
+        mail(subject: String!, reciever: String!): Mail
+    }
+
+    type Mutation {
+        mail(subject: String!, reciever: String!, content: String!): Mail
+    }
+
+    type Mail {
+        subject: String
+        reciever: String
+        content: String
+        _id: String
+    }
 `
 
 module.exports = makeExecutableSchema( { typeDefs, resolvers })
