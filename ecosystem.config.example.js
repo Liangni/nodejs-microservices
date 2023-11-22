b71ca80b3d408080b32aa2d6714470ae976d9997
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2023-11-22 21:56:55 +0800

    add mailing service

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -3,24 +3,38 @@ const path = require('path')
 const basePath = path.join(__dirname, '/packages')
 
 module.exports = {
-  apps : [{
-    name: 'Gateway',
-    script: basePath + '/gateway/server.js',
-    watch: true,
-    env: {
-      PORT: '<YOUR_GATEWAY_PORT>',
-      SERVICE_DB_PORT: '<YOUR_DB_SERVICE_PORT>',
-      Q_URI: '<YOUR_Q_URI>'
-    }
-  }, {
-    name: 'DB Service',
-    script: basePath + '/database_service/server.js',
-    watch: true,
-    env: {
-      PORT: '<YOUR_DB_SERVICE_PORT>',
-      MONGO_URI:'<YOUR_MONGO_URI>'
-    }
-  }],
+    apps : [
+        // API GATEWAY
+        {
+            name: 'Gateway',
+            script: basePath + '/gateway/server.js',
+            watch: true,
+            env: {
+            PORT: '<YOUR_GATEWAY_PORT>',
+            SERVICE_DB_PORT: '<YOUR_DB_SERVICE_PORT>',
+            Q_URI: '<YOUR_Q_URI>'
+            }
+        }, 
+    // DB SERVICE
+        {
+            name: 'DB Service',
+            script: basePath + '/database_service/server.js',
+            watch: true,
+            env: {
+            PORT: '<YOUR_DB_SERVICE_PORT>',
+            MONGO_URI:'<YOUR_MONGO_URI>'
+            }
+        },
+        // MAILING SERVICE
+        {
+            name: 'Mailing Service',
+            script: basePath + '/mailing_service/index.js',
+            watch: true,
+            env: {
+            Q_URI: '<YOUR_Q_URI>'
+            }
+        }
+    ],
 
   // deploy : {
   //   production : {
---
+++ packages/mailing_service/Q/consume.js
@@ -0,0 +1,35 @@
+const amqp = require('amqplib/callback_api')
+const { q: { uri } } = require('../config')
+
+module.exports = () => {
+    const q = 'test_q'
+
+    amqp.connect(uri, (err, conn) => {
+        if (err) throw new Error(err)
+    
+        conn.createChannel((err, ch) => {
+            if (err) throw new Error(err)
+    
+            ch.assertQueue(q, { durable: true })
+    
+            ch.consume(q, (msg) => {
+                let mail
+
+                try {
+                    mail = JSON.parse(msg.content.toString())
+                } catch (err) {
+                    console.log(err)
+
+                    mail = msg.content.toString()
+                }
+    
+                console.log('I RECEIVED A MAIL!', mail)
+    
+                ch.ack(msg) // 告訴 server 確認收到 msg 了
+    
+            }, { noAck: false }) // 告訴 server 要等到 consumer 確認收到 msg 才可以將 msg 從 Q 裡刪掉
+        })
+    })
+}
+
+
